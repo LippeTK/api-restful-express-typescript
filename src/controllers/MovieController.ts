693665eab192dda63a99c4648dd960fa0ebccaa5
Felipe <104037790+LippeTK@users.noreply.github.com>
2025-07-22 20:41:04 -0300

    add getMovieById and validations

--- src/controllers/MovieController.ts
+++ src/controllers/MovieController.ts
@@ -1,12 +1,26 @@
 import { Request, Response } from "express";
 import { movieModel } from "../models/MovieModel";
 import Logger from "../../config/logger";
+import { ModifiedPathsSnapshot } from "mongoose";
 
 export async function createMovie(req: Request, res: Response) {
   try {
     const data = req.body;
     const movie = await movieModel.create(data);
     res.status(201).json(movie);
+  } catch (error: any) {
+    Logger.error(`Erro: ${error.message}`);
+  }
+}
+
+export async function getMovieById(req: Request, res: Response) {
+  try {
+    const id = req.params.id;
+    const movie = await movieModel.findById(id);
+    if (!movie) {
+      return res.status(404).json({ error: "O filme não existe!" });
+    }
+    return res.status(200).json(movie);
   } catch (error: any) {
     Logger.error(`Erro: ${error.message}`);
   }
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { Router, Request, Response } from "express";
-import { createMovie } from "./controllers/MovieController";
-
+import { createMovie, getMovieById } from "./controllers/MovieController";
+import { validate } from "./middleware/handleValidation";
+import { movieCreateValidation } from "./middleware/movieValidation";
 const router = Router();
 
 export default router
   .get("/test", (req: Request, res: Response) => {
     res.status(200).send("Api Funcionando!");
   })
-  .post("/movie", createMovie);
+  .post("/movie", movieCreateValidation(), validate, createMovie)
+  .get("/movie/:id", getMovieById);
