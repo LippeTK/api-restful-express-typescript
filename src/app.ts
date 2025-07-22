f2f52e3ecedf7522a99d19a8a7ce6b64a2113296
Felipe <104037790+LippeTK@users.noreply.github.com>
2025-07-22 13:57:38 -0300

    feat: add http logs with morgan

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,15 @@ import config from "config";
 import express from "express";
 import router from "./router";
 import db from "../config/db";
-
 import Logger from "../config/logger";
+import morganMiddleware from "./middleware/morganMiddleware";
+
 const app = express();
 const port = config.get<number>("port");
 
-//middleware json
+//middlewares
+app.use(morganMiddleware);
 app.use(express.json());
-
-//routes
 app.use("/api/", router);
 
 app.listen(port, async () => {
