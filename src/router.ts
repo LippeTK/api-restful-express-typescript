10ecf5fa438409902dbd16887c29fed5e3a0f05c
Felipe <104037790+LippeTK@users.noreply.github.com>
2025-07-22 15:48:05 -0300

    feat: add controller

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,10 @@
 import { Router, Request, Response } from "express";
+import { createMovie } from "./controllers/MovieController";
 
 const router = Router();
 
-export default router.get("/test", (req: Request, res: Response) => {
-  res.status(200).send("Api Funcionando!");
-});
+export default router
+  .get("/test", (req: Request, res: Response) => {
+    res.status(200).send("Api Funcionando!");
+  })
+  .post("/movie", createMovie);
